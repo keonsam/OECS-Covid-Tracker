0958cc5a65c19a1e475493be3616a3b19fc8de33
keonsam <keonsamuel95@gmail.com>
2021-06-15 21:07:27 -0400

    add feat: navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import "./Navbar.css"
+
+const Navbar = ({searchValue, handleChange, handleSubmit}) => {
+    return (
+        <nav>
+            <div className="container">
+                <h1>OECS Covid Tracker</h1>
+                <form role='search' onSubmit={handleSubmit}>
+                    <label htmlFor='header-search'>
+                        <span className='visually-hidden'>Search</span>
+                    </label>
+
+                    <input className="search-field" type='search' id='header-search' value={searchValue} onChange={handleChange}/>
+
+                    <button type='submit' className='visually-hidden'>
+                    Search
+                    </button>
+                </form>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
